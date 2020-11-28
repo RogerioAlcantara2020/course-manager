98f7635ada8cd458c2c779dd3c858a095ff0c0ad
Rogerio Alcantara <carpe@carpetecnologia.com.br>
2020-11-28 08:09:50 -0300

    Correções

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -6,29 +6,24 @@ import { CourseService } from './course.service';
 @Component({
     templateUrl: './course-info.component.html'
 })
-
 export class CourseInfoComponent implements OnInit {
 
-    course: number = 0;
+    course: Course;
 
     constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
-
-    ngOnInit(): void {
-
-        // this.courseId =+this.activatedRoute.params.subscribe((params)=>{
-        //     this.courseId =+params.get('id')
-        // })
-        //   parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
-         this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
-             next: course => this.course = course,
-             error: err => console.log('Error', err)
-         });
-
-    }
-    save(): void {
-        this.courseService.save(this.course).subscribe({
-            next: course =>console.log('Saved with success', course),
+    
+    ngOnInit(): void { 
+        this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
+            next: course => this.course = course,
             error: err => console.log('Error', err)
         });
     }
+
+    save(): void {
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved with success', course),
+            error: err => console.log('Error', err)
+        });
+    }
+
 }
